a0db001f2ddacf3a25f1ce1c46de7b8d2f1eb0a1
Ryan Harmuth <ryanharmuth@gmail.com>
2016-02-14 01:54:43 +0200

    Dynamic rendering of todo items and adding a new todo

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@
 	      <li className="completed">
 			<div className="view">
 				<input className="toggle" type="checkbox" checked />
-				<label>Taste JavaScript</label>
+				<label>{this.props.children}</label>
 				<button className="destroy"></button>
 			</div>
 			<input className="edit" value="Create a TodoMVC template" />
@@ -17,15 +17,19 @@
 	});
 
 	var TodoList = React.createClass({
-	  render: function() {
-	    return (
-	      	<section className="main">
-				<input className="toggle-all" type="checkbox" />
-				<ul className="todo-list">
-					<Todo />
-					<Todo />
-				</ul>
-			</section>
+	  	render: function() {
+	  		var todoNodes = this.props.todos.map(function(todo) {
+	  			return (
+	  				<Todo key={todo.id} isCompleted={todo.isCompleted}>{todo.text}</Todo>
+	  			)
+	  		});
+	    	return (
+		      	<section className="main">
+					<input className="toggle-all" type="checkbox" />
+					<ul className="todo-list">
+						{todoNodes}
+					</ul>
+				</section>
 	    );
 	  }
 	});
@@ -53,22 +57,53 @@
 	});
 
 	var Header = React.createClass({
+		getInitialState: function() {
+			return {text:""};
+		},
+		handleTodoAdd: function(e) {
+			console.log('form todo add');
+			var newTodo = { text: e.target.value, isCompleted: false };
+			this.props.onTodoAdd(newTodo);
+			this.setState({text:""});
+		},
+		handleChange: function(e) {
+			console.log('la change');
+			this.setState({text: e.target.value});
+		},
+		handleKeyPress: function(e) {
+			console.log('la enter');
+			if (e.key === 'Enter') {
+	      		this.handleTodoAdd(e)
+	      	}
+		},
 	 	render: function() {
 	    	return (
 	      		<header className="header">
 				<h1>todos</h1>
-				<input className="new-todo" placeholder="What needs to be done?" autofocus />
+				<input className="new-todo" placeholder="What needs to be done?" autofocus onKeyPress={this.handleKeyPress} onChange={this.handleChange} value={this.state.text} />
 				</header>
 	    	);
 	  	}
 	});
 
 	var TodoBox = React.createClass({
+		getInitialState: function() {
+			return { todos: [{id: 1, text:"Todo1", isCompleted: true}, {id: 2, text:"Todo2", isCompleted: false}] };
+		},
+		handleTodoAdd: function(newTodo) {
+			console.log('box todo add');
+			var currentTodos = this.state.todos;
+			var lastId = currentTodos[currentTodos.length-1].id;
+			var newTodos = currentTodos.concat([{ id: lastId+1, text: newTodo.text, isCompleted: newTodo.isCompleted }]);
+			console.log(newTodos);
+			this.setState({todos: newTodos});
+		},
   		render: function() {
+  			console.log('todo box render');
     		return (
     			<section className="todoapp">
-	      			<Header />
-	      			<TodoList />
+	      			<Header onTodoAdd={this.handleTodoAdd} />
+	      			<TodoList todos={this.state.todos}/>
 	      			<Footer />
 	      		</section>
     		);
